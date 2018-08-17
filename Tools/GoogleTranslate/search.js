2e88a0e35d9a32c367f3d7c3ee8f79e632020163
Thach <thachvn84@gmail.com>
2018-08-18 01:15:57 +0900

    modify search.js

--- Tools/GoogleTranslate/search.js
+++ Tools/GoogleTranslate/search.js
@@ -1,28 +1,33 @@
 const translate = require('google-translate-api');
 
 var fs = require("fs");
-var content = fs.readFileSync("retrans.txt", "utf8");
-
-translate("感じ", { from: 'ja', to: 'vi' }).then(res => {
-    console.log(res.text);
-    return 0;
-}).catch(err => {
-    //console.error(err);
-    return 0;
-});
+var content = fs.readFileSync("unknown.txt", "utf8");
 
 var lines = content.split("\n");
+var start = 183624;
+//159624
+//163624
+//167624
+//171624
+//175624
+//179624
+//183624
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
-var step = lines.length / 10;
-var block = 5;
-var stop = step * (block + 1);
-var start = (step * block % 2 == 0) ? step * block : step * block + 1
-var stop = step * (block + 1);
-start = Math.round(start);
-stop = Math.round(stop);
-console.log(start + "; " + stop);
-for (var i = start; i < stop; i = i + 2) {
+
+
+var count = 0;
+for (var i = start; i < lines.length; i = i + 2) {
     if (lines[i] != "--OK--") {
+        count++;
         translate(lines[i] + "【" + (i + 1).toString() + "】", { from: 'ja', to: 'vi' }).then(res => {
             console.log(res.text);
             return 0;
@@ -31,5 +36,7 @@ for (var i = start; i < stop; i = i + 2) {
             return 0;
         });
     }
+    if (count > 2000) break;
 }
+console.log("next start: " + i.toString());
 console.log("completed!----------------------------------")
