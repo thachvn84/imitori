c32971054ebf2972f99ba2fe54cd0a5b2e255cff
Thach <thachvn84@gmail.com>
2018-08-23 01:04:16 +0900

    update overview to avoid warning

---
+++ Source/AdminFE/src/pages/Overview.vue
@@ -0,0 +1,17 @@
+<template>
+</template>
+<script>
+import axios from 'axios';
+import vueResource from 'vue-resource'
+import Vue from 'vue'
+
+Vue.use(vueResource);
+
+export default {
+   
+}
+</script>
+
+<style>
+
+</style>
