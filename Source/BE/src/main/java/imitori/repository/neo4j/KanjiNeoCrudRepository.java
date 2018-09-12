96a2076bffd2e59a1aff61cc2b1cf2425cebdc90
Thach <thachvn84@gmail.com>
2018-09-13 08:19:34 +0900

    Add new repository and mobile frontend

--- Source/BE/src/main/java/imitori/repository/neo4j/KanjiNeoCrudRepository.java
+++ Source/BE/src/main/java/imitori/repository/neo4j/KanjiNeoCrudRepository.java
@@ -1,5 +1,9 @@
 package imitori.repository.neo4j;
 
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+
+import imitori.entity.neo4j.KanjiNeoEntity;
+
 public interface KanjiNeoCrudRepository extends Neo4jRepository<KanjiNeoEntity, Integer> {
     public Integer searchWord(String s);
 }
--- Source/BE/src/main/java/imitori/repository/neo4j/WordNeoCrudRepository.java
+++ Source/BE/src/main/java/imitori/repository/neo4j/WordNeoCrudRepository.java
@@ -1,5 +1,9 @@
 package imitori.repository.neo4j;
 
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+
+import imitori.entity.neo4j.WordNeoEntity;
+
 public interface WordNeoCrudRepository extends Neo4jRepository<WordNeoEntity, Integer> {
     public Integer searchWord(String s);
 }
